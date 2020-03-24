641b8b3572bc0769bf11f09cfeb98adfae430e52
Nova-cjp <48847601+Nova-cjp@users.noreply.github.com>
2020-03-25 00:02:37 +0800

    WC-CMAKE项目（不含UI界面）

---
+++ WC_without_UI/cmake-build-debug/CMakeFiles/WC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WC.dir/main.cpp.obj"
+  "WC.exe"
+  "WC.exe.manifest"
+  "WC.lib"
+  "WC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
